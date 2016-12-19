617e4c6fd248a82e3c58366c92765c8e29bf0a6d
Roee FL <roeefl@gmail.com>
2016-12-20 00:04:44 +0200

    Some JS and CSS modifications, added angular-route

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
-var mainModule = angular.module('chromicRelief', [
-  'chromicRelief.controllers'
-]);
+var mainApp = angular.module('chromicRelief', []);
 
-mainModule.config([
+mainApp.config([
   '$compileProvider',
   function ($compileProvider) {
       //  Default imgSrcSanitizationWhitelist: /^\s*((https?|ftp|file|blob):|data:image\/)/
@@ -10,3 +8,104 @@ mainModule.config([
       $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|ftp|file|blob|chrome-extension):|data:image\/)/);
   }
 ]);
+
+mainApp.controller('mainController', function($scope) {
+      $scope.filters = { };
+
+      $scope.reliefList = [
+        {
+          id: "parse-xkcd",
+          alt: "xkcd",
+          src: "xkcd",
+          category: "comic"
+        },
+        {
+          id: "parse-explosm",
+          alt: "Cyanide and Happiness",
+          src: "explosm",
+          category: "comic"
+        },
+        {
+          id: "parse-poorly",
+          alt: "Poorly Drawn Lines",
+          src: "poorly_drawn_lines",
+          category: "comic"
+        },
+        {
+          id: "parse-dirk",
+          alt: "Dirks Big Bunnies",
+          src: "dirks",
+          category: "comic"
+        },
+        {
+          id: "parse-geekandpoke",
+          alt: "Geek and Poke",
+          src: "geekandpoke",
+          category: "comic"
+        },
+        {
+          id: "parse-awkwardzombie",
+          alt: "Awkward Zombie",
+          src: "awkwardzombie",
+          category: "comic"
+        },
+        {
+          id: "parse-smbc",
+          alt: "Saturday Morning Breakfast Cereal",
+          src: "smbc",
+          category: "comic"
+        },
+        {
+          id: "parse-incidental",
+          alt: "Incidental Comics",
+          src: "incidental",
+          category: "comic"
+        },
+        {
+          id: "parse-awkwardyeti",
+          alt: "The Awkward Yeti",
+          src: "awkwardyeti",
+          category: "comic"
+        },
+        {
+          id: "parse-doghouse",
+          alt: "The Doghouse Diaries",
+          src: "doghouse",
+          category: "comic"
+        },
+        {
+          id: "parse-dailykitten",
+          alt: "The Daily Kitten",
+          src: "dailykitten",
+          category: "animalgif"
+        }
+        // {
+        //   id: "parse-abtrusegoose",
+        //   alt: "Abtruse Goose",
+        //   src: "abtrusegoose"
+        // }
+        /* {
+          id: "parse-oatmeal",
+          alt: "The Oatmeal",
+          src: "oatmeal"
+        } */
+      ];
+
+      $scope.filterList = [
+        {
+          id: "filter-all",
+          title: "Show All",
+          categories: ""
+        },
+        {
+          id: "filter-comics",
+          title: "Web Comics",
+          categories: "comic"
+        },
+        {
+          id: "filter-animals",
+          title: "Animal GIFs",
+          categories: "animalgif"
+        }
+      ];
+});
